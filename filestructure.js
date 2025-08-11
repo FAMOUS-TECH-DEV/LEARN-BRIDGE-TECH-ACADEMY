5d5a58d54b167a3ef943f19c6c07bf04d129a695
FAMØUS DEV 👑 <whitebos2.0@gmail.com>
2025-08-11 15:30:45 +0100

    Add files via upload

---
+++ filestructure.js
@@ -0,0 +1,8 @@
+learnbridge/
+       index.html
+       styles.css
+       script.js
+       images/
+         logo.png (placeholder for logo image)
+         logo2.jpg (placeholder for second logo image)
+         testimonials-avaters/(placeholder for testimonial avatars)
